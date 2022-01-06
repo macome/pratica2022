972e82f887e7b0a1dfa715f50b5bd2a47dfd1ee3
macome <alymacome@gmail.com>
2022-01-06 19:13:25 +0200

    Alteracao da imagem do fundo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,16 @@
 @section('title',' The jungle Restaurante e Bar')
 
 @section('content')
-    <h1>Site em construcao</h1>
+
+    <div class="container">
+        <div class="text-center">
+            <img src="/img/tirando o fundo.png" alt="Manutencao">
+        </div>
+        
+        <h1 class="text-center pa-md">Site em construcao</h1>
+    </div>
+    <!-- <h1>Site em construcao</h1> -->
     
-    <img src="/img/fundo em manutencao.jpg" alt="Manutencao">
+    <!-- <img src="/img/fundo em manutencao.jpg" alt="Manutencao"> -->
 
 @endsection    
